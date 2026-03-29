bdaa91b5cd2a11e0537a54c1445b7aa4163daaa5
gersonaguiartech <gersonaguiar.tech@gmail.com>
2026-03-29 17:51:01 -0300

    feat: finaliza desenvolvimento da atividade 5

---
+++ atv05.py
@@ -0,0 +1,35 @@
+def verificar_quadrado_magico(matriz):
+    # Primeiro, calculamos a soma da primeira linha para usar como nosso "alvo"
+    soma_alvo = sum(matriz[0])
+    
+    # Verificando as linhas
+    for linha in matriz:
+        if sum(linha) != soma_alvo:
+            return False
+            
+    # Verificando as colunas
+    for coluna in range(3):
+        soma_coluna = matriz[0][coluna] + matriz[1][coluna] + matriz[2][coluna]
+        if soma_coluna != soma_alvo:
+            return False
+            
+    # Verificando as diagonais
+    diagonal_principal = matriz[0][0] + matriz[1][1] + matriz[2][2]
+    diagonal_secundaria = matriz[0][2] + matriz[1][1] + matriz[2][0]
+    
+    if diagonal_principal != soma_alvo or diagonal_secundaria != soma_alvo:
+        return False
+        
+    return True
+
+# Matriz do exercício
+matriz_exercicio = [
+    [8, 0, 7], # [cite: 48]
+    [4, 5, 6], # [cite: 48]
+    [3, 10, 2] # [cite: 48]
+]
+
+if verificar_quadrado_magico(matriz_exercicio):
+    print("Que legal! Esta matriz É um quadrado mágico.")
+else:
+    print("Poxa, esta matriz NÃO é um quadrado mágico.")
