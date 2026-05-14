6e3204d37dc4bdadcdee56762cf381f4ccbbfa1f
gersonaguiartech <gersonaguiar.tech@gmail.com>
2026-05-14 10:17:59 -0300

    atualizando essa atv5

--- Filas/atv5.py
+++ Filas/atv5.py
@@ -9,21 +9,16 @@ def calcular_estatisticas(colecao):
     for numero in colecao:
         if numero > maior:
             maior = numero
-            
-        # Verifica o menor
+
         if numero < menor:
             menor = numero
             
-        # Adiciona o número à soma total
         soma += numero
 
-    # Calcula a média aritmética
     media = soma / len(colecao)
 
-    # Retorna os três valores calculados
     return maior, menor, media
 
-# --- Exemplo de Uso ---
 minha_fila = [12, 5, -3, 8, 20, 1]
 maior_val, menor_val, media_val = calcular_estatisticas(minha_fila)
 
